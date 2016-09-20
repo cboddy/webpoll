35a4d3605402b1f7b9ef1263274fa8f1dc7c6e2d
Chris <chris@boddy.im>
2016-09-20 22:22:15 +0100

    applied pep8 formatter to webpoll

--- webpoll/tests/webpoll_tests.py
+++ webpoll/tests/webpoll_tests.py
@@ -7,6 +7,7 @@ import webpoll
 
 hackernews_html = "hackernews_test.html"
 
+
 class WebPollTest(unittest.TestCase):
 
     def test_filter_links(self):
@@ -14,12 +15,14 @@ class WebPollTest(unittest.TestCase):
             html_page = "".join(f.readlines())
         links = webpoll.filter_links(html_page)
         self.assertEquals(227, len(links))
-        self.assertIn("https://icomp.de/shop-icomp/en/33/items/commodore-back-in-germany.html", links)
+        self.assertIn(
+            "https://icomp.de/shop-icomp/en/33/items/commodore-back-in-germany.html", links)
 
     def test_msg_builder(self):
-        notifications = [webpoll.Notification(url, link) for (url,link) in [("a", "b"), ("c", "d")] ]
+        notifications = [webpoll.Notification(url, link) for (url, link) in [
+            ("a", "b"), ("c", "d")]]
         msg_body = webpoll.msg_body_builder(notifications, "now")
-        
+
         expected_body = """Hello, as of now
 
 The web page a links to b.
@@ -29,4 +32,4 @@ Chur,
 Webpoll
 """
         self.assertEquals(expected_body, msg_body)
-        self.assertRaises(AssertionError, webpoll.msg_body_builder,  [], "now") 
+        self.assertRaises(AssertionError, webpoll.msg_body_builder,  [], "now")
--- webpoll/webpoll.py
+++ webpoll/webpoll.py
@@ -5,15 +5,18 @@ import time
 import threading
 import datetime
 import smtplib
-import os, ConfigParser
+import os
+import ConfigParser
 import collections
 
 Notification = collections.namedtuple("Notification", ["url", "link"])
+
 
 def fetch(url):
     """make an HTTP GET request and return the data @ url"""
     req = urllib2.Request(url)
     return urllib2.urlopen(req).read()
+
 
 def filter_links(html_page):
     """
@@ -23,6 +26,7 @@ def filter_links(html_page):
     """
     soup = BeautifulSoup(html_page, "html.parser")
     return [link.get("href") for link in soup.find_all("a")]
+
 
 def msg_body_builder(notifications, timestamp):
     """
@@ -34,9 +38,9 @@ def msg_body_builder(notifications, timestamp):
     """
     assert(len(notifications) > 0)
 
-    notification_msg = "\n".join(["The web page {} links to {}.".format(n.url, n.link)\
-            for n in notifications])
-    
+    notification_msg = "\n".join(["The web page {} links to {}.".format(n.url, n.link)
+                                  for n in notifications])
+
     return """Hello, as of {}
 
 {} 
@@ -47,8 +51,9 @@ Webpoll
 
 
 class WebPoll(object):
-    def __init__(self, target_urls, target_links, poll_interval, 
-            email_server_address, email_user, email_password, email_targets):
+
+    def __init__(self, target_urls, target_links, poll_interval,
+                 email_server_address, email_user, email_password, email_targets):
         """
         args: 
             target_urls: a list of urls to poll
@@ -60,12 +65,13 @@ class WebPoll(object):
             defaults to [email_user]
         """
         self.target_urls = target_urls
-        self.target_links =  [re.compile(regex) for regex in target_links]
+        self.target_links = [re.compile(regex) for regex in target_links]
         self.poll_interval = poll_interval
         self.email_server_address = email_server_address
-        self.email_user =  email_user
+        self.email_user = email_user
         self.email_password = email_password
-        self.email_targets = [email_user] if not email_targets else email_targets
+        self.email_targets = [
+            email_user] if not email_targets else email_targets
         self.lock = threading.Lock()
         self._is_finished = False
 
@@ -77,7 +83,7 @@ class WebPoll(object):
     @is_finished.setter
     def is_finished(self, is_finished):
         with self.lock:
-            self._is_finished = is_finished 
+            self._is_finished = is_finished
 
     def close(self):
         self.is_finished = True
@@ -90,16 +96,15 @@ class WebPoll(object):
         """
         pages = [fetch(url) for url in self.target_urls]
 
-        notifications = [Notification(url, link)  \
-                for regex in self.target_links\
-                for link in filter_links(page)\
-                for page in  pages\
-                if regex.search(link)]
-        
+        notifications = [Notification(url, link)
+                         for regex in self.target_links
+                         for link in filter_links(page)
+                         for page in pages
+                         if regex.search(link)]
+
         if notifications:
             email_body = msg_body_builder(notifications, now.isoformat)
             self.notify_by_email(email_body)
-
 
     def notify_by_email(self, msg_body):
         """
@@ -122,16 +127,17 @@ class WebPoll(object):
             sleep_seconds = min(0, self.poll_interval - time_delta.seconds)
             time.sleep(sleep_seconds)
 
+
 def main():
     config = ConfigParser.ConfigParser()
     config.read(["app.cfg", os.path.expanduser("~/.webpoll.cfg")])
     get_param = lambda x, default: config.get("webpoll", x, default)
-    get_list_param = lambda x: get_param(x, None).split(",") 
+    get_list_param = lambda x: get_param(x, None).split(",")
 
     webpoll = WebPoll(
         get_list_param("links"),
         get_list_param("link_regexs"),
-        get_param("poll_interval", 60*30),
+        get_param("poll_interval", 60 * 30),
         get_param("email_server_address", "smtp.gmail.com:587"),
         get_param("email_user", None),
         get_param("email_password", None),
